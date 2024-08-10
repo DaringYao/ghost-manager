603f3d655f2f01345475bc76e505176b4be08c34
yaolisheng <1935743565@qq.com>
2024-08-11 01:42:29 +0800

    job

---
+++ ghost-job/job-cache/src/main/java/com/ghost/job/cache/Main.java
@@ -0,0 +1,14 @@
+package com.ghost.job.cache;
+
+/**
+ * @author yaolsœ
+ * @version 1.0
+ * @since 2024/8/11 01:37
+ */
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
